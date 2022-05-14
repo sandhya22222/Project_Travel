b65d29e038901c6bab66d263c882ec5a2716b2ec
“[firstname <allu.sandhya025@gmail.com>
2022-05-15 00:03:18 +0530

    added hotellist

--- Travel-angular/src/app/routing/hotelclick/hotelclick.component.ts
+++ Travel-angular/src/app/routing/hotelclick/hotelclick.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { SignupService } from 'src/app/services/signup.service';
 
 @Component({
   selector: 'app-hotelclick',
@@ -6,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hotelclick.component.css']
 })
 export class HotelclickComponent implements OnInit {
+  calculateAmount = 0;
+  isGetAmtClicked = false;
+  passengers: number = 1;
 
-  constructor() { }
+  ticketPrize: number = 15940;
+  isUserAdded: boolean = false;
+
+  constructor(private signService: SignupService, private myhttp: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  getFormsValue(formRef: any) {
+
+    let data = {
+     
+      firstname: formRef.value.fname,
+      lastname: formRef.value.lname,
+      email: formRef.value.email,
+      phonenumber: formRef.value.phonenumber,
+      city: formRef.value.city,
+      hotelname: formRef.value.hotelname,
+      passengers: formRef.value.passengers,
+      checkin: formRef.value.checkin,
+      checkout: formRef.value.checkout,
+      rooms:formRef.value.rooms,
+      amount: this.calculateAmount,
+      // date: formRef.value.date,
+    };
+
+    this.myhttp.post('/api/users/hotelclick', data, { responseType: 'text' })
+      .subscribe(data => {
+        console.log(data);
+        this.isUserAdded = true;
+        formRef.form.reset();
+      });
+  }
+
+  getValues() {
+     alert("Payment done successfully")
+  }
+  getAmount() {
+
+    this.isGetAmtClicked = true;
+
+    this.calculateAmount = this.passengers * this.ticketPrize
+  }
 }
+
--- Travel-angular/src/app/routing/tohotels/tohotels.component.ts
+++ Travel-angular/src/app/routing/tohotels/tohotels.component.ts
@@ -11,37 +11,43 @@ export class TohotelsComponent implements OnInit {
     img: 'https://r1imghtlak.mmtcdn.com/91091b3ee33811e9a3130242ac110003.jpg?&output-quality=75&downsize=243:162&crop=243:162;0,10&output-format=webp',
     hotelname:'FOUR SEASON HOTEL',
     checkin:'Free Cancellation',
-    rating:'500+guests like you rated stay Very Good'
+    rating:'500+guests like you rated stay Very Good',
+    cost:'15,750'
   },
   {
     img: 'https://r1imghtlak.mmtcdn.com/4b0c9f2a709911e7add5025f77df004f.jpg?&output-quality=75&downsize=243:162&output-format=webp',
     hotelname:'SVENSKA DESIGN HOTEL',
     checkin:'Free Cancellation till 24hrs before checkin',
-    rating:'400+guests like you rated stay Very Good'
+    rating:'400+guests like you rated stay Very Good',
+    cost:'10,500'
   },
   {
     img: 'https://r1imghtlak.mmtcdn.com/7a697c646db211e7aa7f0a4cef95d023.jpg?&output-quality=75&downsize=243:162&output-format=webp',
      hotelname:'HOTEL SUBA INTERNATIONAL',
      checkin:'Free Cancellation till 24hrs before checkin',
-     rating:'500+guests like you rated stay Very Good'
+     rating:'500+guests like you rated stay Very Good',
+     cost:'6,999'
   },
   {
     img: 'https://r1imghtlak.mmtcdn.com/89d4ecde9a1e11e4aab032e76f7e45c9.jfif?&output-quality=75&downsize=243:162&crop=243:162;40,0&output-format=webp',
     hotelname:'THE OBEROI HOTEL',
     checkin:'Free Cancellation till 24hrs before checkin',
-    rating:'300+guests like you rated stay Very Good'
+    rating:'300+guests like you rated stay Very Good',
+    cost:'19,500'
   },
   {
     img: 'https://r1imghtlak.mmtcdn.com/c283c8e459af11e8b5860a9df65c8753.jpg?&output-quality=75&downsize=243:162&output-format=webp',
     hotelname:'THE TAJ MAHAL PALACE',
     checkin:'Free Cancellation till checkin',
-    rating:'500+guests like you rated stay Very Good'
+    rating:'500+guests like you rated stay Very Good',
+    cost:'11,250'
   },
   {
     img: 'https://r2imghtlak.mmtcdn.com/r2-mmt-htl-image/htl-imgs/200701101227074979-1b72892a8c4211e9898a0242ac110003.jpg?&output-quality=75&downsize=243:162&crop=243:162;22,0&output-format=webp',
     hotelname:'HOTEL GODWIN',
     checkin:'Free Cancellation till 24hrs before checkin',
-    rating:'400+guests like you rated stay Very Good'
+    rating:'400+guests like you rated stay Very Good',
+    cost:'6,351'
   },
   ]
 
